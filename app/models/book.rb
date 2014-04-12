69ee19982ccc5ddee8fbfbd0d13254e31d122e13
Tran Xuan Vu <xuanvu442@gmail.com>
2014-04-12 18:34:00 +0700

    chinh sua va them search book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ActiveRecord::Base
-	has_many :category
-	has_attached_file :image, :styles => { :medium => "500x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_many :categories
+	has_attached_file :image, :styles => { :medium => "500x500#", :thumb => "320x240#" }, :default_url => "/images/:style/missing.png"
    	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	def self.search(search)
+		where("Title like ?", "%#{search}%")
+	end
 end
